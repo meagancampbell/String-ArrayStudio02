fb5cac0401278ae1ee4499d56d7203318f559b54
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-09 19:10:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let protoArray1 = "3,6,9,12";
+let protoArray2 = "A;C;M;E";
+let protoArray3 = "space delimited string";
+let protoArray4 = "Comma-spaces, might, require, typing, caution";
+
+strings = [protoArray1, protoArray2, protoArray3, protoArray4];
+
+//a) Use the 'includes' method to check to see if the words in each string are separated by commas (,), semicolons (;) or just spaces.
+
+console.log(protoArray1.includes(","));
+console.log(protoArray2.includes(";"));
+console.log(protoArray3.includes(" "));
+console.log(protoArray4.includes(","));
+
+//b) If the string uses commas to separate the words, split it into an array, reverse the entries, and then join the array into a new comma separated string.
+
+let newArray1 = protoArray4.split(", ");
+console.log(newArray1);
+
+const reverse = newArray1.reverse();
+
+console.log(reverse );
+
+//c) If the string uses semicolons to separate the words, split it into an array, alphabetize the entries, and then join the array into a new comma separated string.
+
+let arr2 = protoArray2.split("");
+console.log(arr2 );
+
+function myFunction() {
+  console.log(arr2.sort());
+  
+}
+myFunction.call(myFunction);
+
+//d) If the string uses spaces to separate the words, split it into an array, reverse alphabetize the entries, and then join the array into a new space separated string.
+
+
+//e) If the string uses ‘comma spaces’ to separate the list, modify your code to produce the same result as part “b”, making sure that the extra spaces are NOT part of the final string.
